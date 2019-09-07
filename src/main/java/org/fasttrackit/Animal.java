44a7a93de2fb78c7bdc3f5796df13335c68703d6
NemesAlexandru <alexandrunemes1@gmail.com>
2019-09-07 20:09:18 +0300

    Adding Classes (no expiry date)

---
+++ src/main/java/org/fasttrackit/Animal.java
@@ -0,0 +1,11 @@
+package org.fasttrackit;
+
+public class Animal {
+    String name;
+    int age;
+    int hungerLevel;
+    int health;
+    int happiness;
+    String favoriteFood;
+    String favoriteActivity;
+}
