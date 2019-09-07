edf0f40fbfa9232fbdc54b407f2e7a3edb38e550
NemesAlexandru <alexandrunemes1@gmail.com>
2019-09-07 20:22:10 +0300

    Adding Classes (no expiry date)

--- src/main/java/org/fasttrackit/Food.java
+++ src/main/java/org/fasttrackit/Food.java
@@ -1,11 +1,8 @@
 package org.fasttrackit;
 
-import java.util.Date;
-
 public class Food {
     String name;
     double price;
     int quantity;
-    Date expiryDate;
     String currentlyAvailable;
 }
