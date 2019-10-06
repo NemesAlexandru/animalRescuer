2330ac073bb437413586cc0a32e05f2ece71bdc3
NemesAlexandru <alexandrunemes1@gmail.com>
2019-10-06 16:12:33 +0300

    Studied If, Else

--- src/main/java/org/fasttrackit/Rescuer.java
+++ src/main/java/org/fasttrackit/Rescuer.java
@@ -26,6 +26,17 @@ public class Rescuer {
     }
 
     public void feed(Animal animal, Food food) {
+        if (animal.getFavoriteFood().equals(food.getName())){
+            System.out.println("The animal loves this food");
+            animal.setHappiness(animal.getHappiness()+1);
+            System.out.println("Animal happiness level is: " + animal.getHappiness());
+        }
+
+        else {
+            System.out.println("Animal hates this food.");
+            System.out.println("Happiness level unchanged: " + animal.getHappiness() + "/10.");
+        }
+
         System.out.println("Current animal hunger level is: " + animal.getHungerLevel() + "/10");
         System.out.println(this.getName() + " just gave some " + food.getName()+ " food to " + animal.getName());
         animal.setHungerLevel(animal.getHungerLevel() + 1);
@@ -33,9 +44,17 @@ public class Rescuer {
     }
 
     public void playFetch (Animal animal, RecreationalActivity recreationalActivity) {
-        System.out.println("Current animal happiness is: " + animal.getHappiness() + "/10");
-        System.out.println(this.getName() + " plays " + recreationalActivity.getName() + " with animal " + animal.getName());
-        animal.setHappiness(animal.getHappiness() + 1);
-        System.out.println("New happiness level for " + animal.getName() + " is: " + animal.getHappiness() + "/10");
+
+        if (animal.getFavoriteActivity().equals(recreationalActivity.getName())) {
+            System.out.println("Current happiness level for " + animal.getName() + " is: " + animal.getHappiness());
+            animal.setHappiness(animal.getHappiness() + 2);
+            System.out.println("New happiness level is: " + animal.getHappiness());
+        }
+        else {
+            System.out.println("Animal hates this food.");
+            animal.setHappiness(animal.getHappiness() + 1);
+            System.out.println("New happiness level is: " + animal.getHappiness());
+        }
     }
-}
+    }
+
